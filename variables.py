bad8ffa8ec9d9cc154aaa812eafa015c91865e55
Abdulkadir <qansax30@hotmail.com>
2024-10-05 00:02:19 +0200

    Python variables

---
+++ variables.py
@@ -0,0 +1,40 @@
+
+# variables in python 
+
+first name = 'Abdulkadir'
+last name = 'Mohamud'
+country = 'The Netherlands'
+city = 'The Hague'
+age = 230
+is_married = not married
+skills = ['HTML', 'CSS' , 'JS', React, 'Python']
+person_info = {
+    'firstname :' 'Abdulkadir',
+    'lastname :'   'Mohamud',
+    'country :'     'The Netherlands',
+    'city:'         'The Hague',
+
+}
+
+# printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name:', last_name)
+print('Last name length:', len(last_name))
+print('Country: ', country)
+print('city:', city)
+print('Married: ', is_married)
+print('Skills: ', skills )
+print('Person information:', person_info)
+
+#Declaring multiple variables in one line 
+
+first_name, last_name, Country, age, is_married = 'Abdulkadir', 'Mohamud', 'The Hague', 230, not married
+
+print(first_name, last_name, country, age, is is_not Married)
+print('First name:', first_name)
+print('Last name : '. last_name)
+print('Country: ', country)
+print('Age:', age)
+print('Not Married: ' is_not married)
